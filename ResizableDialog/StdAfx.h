946c8fda9d45e296e22205829dcf760d1e21c70d
paolo <>
2000-10-07 15:29:57 +0000

    fix for VC5 in stdafx.h

--- ResizableDialog/StdAfx.h
+++ ResizableDialog/StdAfx.h
@@ -11,10 +11,9 @@
 #endif // _MSC_VER > 1000
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
-#define OEMRESOURCE
+
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
