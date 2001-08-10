991ac91d763516ddb02393d43052da2917b2cf85
ppescher <ppescher@hotmail.com>
2001-08-10 00:20:06 +0000

    fixed sizing border bug (client area preserved now)

--- ResizableLib/ResizableDialog.cpp
+++ ResizableLib/ResizableDialog.cpp
@@ -74,8 +74,15 @@ int CResizableDialog::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	if (CDialog::OnCreate(lpCreateStruct) == -1)
 		return -1;
 
-	// automatically set resizable style
-	ModifyStyle(DS_MODALFRAME, WS_POPUP | WS_THICKFRAME, SWP_FRAMECHANGED);
+	// keep client area
+	CRect rect;
+	GetClientRect(&rect);
+	// set resizable style
+	ModifyStyle(DS_MODALFRAME, WS_POPUP | WS_THICKFRAME);
+	// adjust size to reflect new style
+	CalcWindowRect(&rect);
+	SetWindowPos(NULL, 0, 0, rect.Width(), rect.Height(), SWP_FRAMECHANGED|
+		SWP_NOMOVE|SWP_NOZORDER|SWP_NOACTIVATE|SWP_NOREPOSITION);
 
 	if (!InitGrip())
 		return -1;
