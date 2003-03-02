611c1f8f0950b77aea5c38fa2018707aa87748b9
ppescher <ppescher@hotmail.com>
2003-03-02 17:13:50 +0000

    updated macro definitions and preprocessor directives

--- ResizableLib/StdAfx.h
+++ ResizableLib/StdAfx.h
@@ -10,17 +10,33 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+// Use target Windows version for compatibility
+// with CPropertyPageEx, CPropertySheetEx
+#define _WIN32_IE 0x0400
+
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// MFC support for Windows Common Controls
-#endif // _AFX_NO_AFXCMN_SUPPORT
 
 #ifndef WS_EX_LAYOUTRTL
 #pragma message("Please update your Windows header files, get the latest SDK")
-#define WS_EX_LAYOUTRTL	0x00400000
+#pragma message("WinUser.h is out of date!")
+
+#define WS_EX_LAYOUTRTL		0x00400000
+#endif
+
+#ifndef WC_BUTTON
+#pragma message("Please update your Windows header files, get the latest SDK")
+#pragma message("CommCtrl.h is out of date!")
+
+#define WC_BUTTON			_T("Button")
+#define WC_STATIC			_T("Static")
+#define WC_EDIT				_T("Edit")
+#define WC_LISTBOX			_T("ListBox")
+#define WC_COMBOBOX			_T("ComboBox")
+#define WC_SCROLLBAR		_T("ScrollBar")
 #endif
 
 
