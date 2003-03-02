ee484bc764ca7cd3cca35b383ab5f351ac75c924
ppescher <ppescher@hotmail.com>
2003-03-02 17:22:09 +0000

    updated to use new WM_GETMINMAXINFO chaining

--- ResizableLib/ResizableMDIChild.cpp
+++ ResizableLib/ResizableMDIChild.cpp
@@ -53,36 +53,9 @@ void CResizableMDIChild::OnGetMinMaxInfo(MINMAXINFO FAR* lpMMI)
 {
 	MinMaxInfo(lpMMI);
 
-	CView* pView = GetActiveView();
-	if (pView == NULL)
-		return;
-
-	// get the extra size from view to frame
-	CRect rectClient, rectWnd;
-	if (IsZoomed())
-		GetClientRect(rectWnd);
-	else
-		GetWindowRect(rectWnd);
-	RepositionBars(0, 0xFFFF, AFX_IDW_PANE_FIRST, reposQuery, rectClient);
-	CSize sizeExtra = rectWnd.Size() - rectClient.Size();
-
-	// ask the view for track size
-	MINMAXINFO mmiView = *lpMMI;
-	pView->SendMessage(WM_GETMINMAXINFO, 0, (LPARAM)&mmiView);
-	mmiView.ptMaxTrackSize = sizeExtra + mmiView.ptMaxTrackSize;
-	mmiView.ptMinTrackSize = sizeExtra + mmiView.ptMinTrackSize;
-
-	// min size is the largest
-	lpMMI->ptMinTrackSize.x = __max(lpMMI->ptMinTrackSize.x,
-		mmiView.ptMinTrackSize.x);
-	lpMMI->ptMinTrackSize.y = __max(lpMMI->ptMinTrackSize.y,
-		mmiView.ptMinTrackSize.y);
-
-	// max size is the shortest
-	lpMMI->ptMaxTrackSize.x = __min(lpMMI->ptMaxTrackSize.x,
-		mmiView.ptMaxTrackSize.x);
-	lpMMI->ptMaxTrackSize.y = __min(lpMMI->ptMaxTrackSize.y,
-		mmiView.ptMaxTrackSize.y);
+	CWnd* pView = GetDlgItem(AFX_IDW_PANE_FIRST);//GetActiveView();
+	if (pView != NULL)
+		ChainMinMaxInfo(lpMMI, this, pView);
 
 	// MDI should call default implementation
 	CMDIChildWnd::OnGetMinMaxInfo(lpMMI);
--- ResizableLib/ResizableMDIFrame.cpp
+++ ResizableLib/ResizableMDIFrame.cpp
@@ -57,29 +57,7 @@ void CResizableMDIFrame::OnGetMinMaxInfo(MINMAXINFO FAR* lpMMI)
 	if (pChild == NULL || !bMaximized)
 		return;
 
-	// get the extra size from child to frame
-	CRect rectChild, rectWnd;
-	GetWindowRect(rectWnd);
-	RepositionBars(0, 0xFFFF, AFX_IDW_PANE_FIRST, reposQuery, rectChild);
-	CSize sizeExtra = rectWnd.Size() - rectChild.Size();
-
-	// ask the child frame for track size
-	MINMAXINFO mmiView = *lpMMI;
-	pChild->SendMessage(WM_GETMINMAXINFO, 0, (LPARAM)&mmiView);
-	mmiView.ptMaxTrackSize = sizeExtra + mmiView.ptMaxTrackSize;
-	mmiView.ptMinTrackSize = sizeExtra + mmiView.ptMinTrackSize;
-
-	// min size is the largest
-	lpMMI->ptMinTrackSize.x = __max(lpMMI->ptMinTrackSize.x,
-		mmiView.ptMinTrackSize.x);
-	lpMMI->ptMinTrackSize.y = __max(lpMMI->ptMinTrackSize.y,
-		mmiView.ptMinTrackSize.y);
-
-	// max size is the shortest
-	lpMMI->ptMaxTrackSize.x = __min(lpMMI->ptMaxTrackSize.x,
-		mmiView.ptMaxTrackSize.x);
-	lpMMI->ptMaxTrackSize.y = __min(lpMMI->ptMaxTrackSize.y,
-		mmiView.ptMaxTrackSize.y);
+	ChainMinMaxInfo(lpMMI, this, pChild);
 
 	// MDI should call default implementation
 	CMDIFrameWnd::OnGetMinMaxInfo(lpMMI);
