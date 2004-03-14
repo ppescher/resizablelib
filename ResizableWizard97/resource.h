a86d80423abab902725d69d95115b725f69754c8
ppescher <ppescher@hotmail.com>
2004-03-14 18:21:18 +0000

    new resources for IE5 style wizards

--- ResizableWizard97/resource.h
+++ ResizableWizard97/resource.h
@@ -5,14 +5,9 @@
 #define IDS_SAMPLEWIZARD                1
 #define IDS_HEADERTITLE                 2
 #define IDS_HEADERSUBTITLE              3
-#define IDM_ABOUTBOX                    0x0010
-#define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
-#define IDD_WIZARD97_DIALOG             102
-#define IDB_BANNER256                   103
-#define IDB_BANNER16                    104
-#define IDB_WATERMARK16                 105
-#define IDB_WATERMARK256                106
+#define IDB_BANNER_OLD                  103
+#define IDB_WATERMARK_OLD               106
 #define IDC_BIGBOLDTITLE                107
 #define IDC_BOLDTITLE                   108
 #define IDC_TRANSPARENT1                109
@@ -24,22 +19,29 @@
 #define IDC_RADIO2                      115
 #define IDD_INTRO_NEW                   116
 #define IDD_INTERIOR1_NEW               117
+#define IDD_INTERIOR1                   117
 #define IDD_INTERIOR2_NEW               118
+#define IDD_INTERIOR2                   118
 #define IDD_COMPLETION_NEW              119
 #define IDR_MAINFRAME                   128
+#define IDB_BANNER_NEW                  129
+#define IDB_WATERMARK_NEW               130
+#define IDD_COMPLETION                  131
+#define IDD_INTRO                       132
 #define IDC_EDIT1                       1000
 #define IDC_STATIC1                     1000
 #define IDC_STATIC2                     1001
 #define IDC_GROUP1                      1002
 #define IDC_STATIC_TITLE                1003
+#define IDC_STATIC3                     1004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
