6fb15db5214c04d4b4d7dfa4283559a225edc44d
ppescher <ppescher@hotmail.com>
2004-03-21 18:30:12 +0000

    added ids for all controls

--- ResizableFormViewMDI/Resource.h
+++ ResizableFormViewMDI/Resource.h
@@ -18,6 +18,7 @@
 #define IDC_GROUP1                      1011
 #define IDC_ICON1                       1012
 #define IDC_RADIO3                      1013
+#define IDC_STATIC2                     1014
 
 // Next default values for new objects
 // 
@@ -25,7 +26,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
